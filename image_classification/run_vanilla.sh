57449847ba4e6bb4f4276a7f737f2a18b882ca4d
raymin0223 <bsmn0223@kaist.ac.kr>
2021-11-01 00:08:34 +0900

    scripts files

---
+++ image_classification/run_vanilla.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# teacher is None
+
+python main.py --device=cuda:0 \
+               --student=wrn-28-2 \
+               --dataset=cifar100 \
+               --batch_size=128 \
+               --num_epochs=200 \
+               --mode=crop \
+               --nesterov \
+               --seed=9999
+               
+echo "finished"
+
